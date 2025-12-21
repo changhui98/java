293de4853474f1759d7923f7be7800779b5bdb0d
changhui98 <net_0102@naver.com>
2025-12-21 22:41:39 +0900

    Study : static 메서드1

---
+++ src/static2/DecoMain1.java
@@ -0,0 +1,13 @@
+package static2;
+
+public class DecoMain1 {
+
+    public static void main(String[] args) {
+
+        String s = "hello java";
+
+        DecoUtil1 deco = new DecoUtil1();
+        System.out.println(deco.deco(s));
+    }
+
+}
