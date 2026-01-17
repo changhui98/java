b5ec3207492c9addb7a71a6ecbf116226df6a390
changhui98 <net_0102@naver.com>
2026-01-17 22:10:34 +0900

    Study : 문제와 풀이

---
+++ src/mid1/lang/immutable/test/ImmutableMyDate.java
@@ -0,0 +1,27 @@
+package mid1.lang.immutable.test;
+
+public class ImmutableMyDate {
+
+    private final int year;
+    private final int month;
+    private final int day;
+
+    public ImmutableMyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public ImmutableMyDate withYear(int newYear) {
+        return new ImmutableMyDate(newYear, this.month, this.day);
+    }
+
+    @Override
+    public String toString() {
+        return "ImmutableMyDate{" +
+            "year=" + year +
+            ", month=" + month +
+            ", day=" + day +
+            '}';
+    }
+}
