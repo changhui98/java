1b771e8bb48a34ad93f1cb5ce06b3df2e6df841f
changhui98 <net_0102@naver.com>
2026-02-04 00:52:36 +0900

    Study : Math, Random 클래스

---
+++ src/mid1/lang/math/RandomMain.java
@@ -0,0 +1,28 @@
+package mid1.lang.math;
+
+import java.util.Random;
+
+public class RandomMain {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+//        Random random = new Random(5); // seed 가 같으면 Random 의 결과가 같다.
+
+        int randomInt = random.nextInt();
+        System.out.println("randomInt = " + randomInt);
+
+        double randomDouble = random.nextDouble();
+        System.out.println("randomDouble = " + randomDouble);
+
+        boolean randomBoolean = random.nextBoolean();
+        System.out.println("randomBoolean = " + randomBoolean);
+
+        // 범위 조회
+        int randomRange1 = random.nextInt(10);// 0 ~ 9 까지 출력
+        System.out.println("randomRange1 = " + randomRange1);
+
+        int randomRange2 = random.nextInt(10) + 1;// 1 ~ 10까지 출력
+        System.out.println("randomRange2 = " + randomRange2);
+    }
+
+}
